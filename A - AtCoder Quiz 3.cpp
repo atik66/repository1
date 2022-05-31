7452bd01984f9222559627ca7adbc5d1652d29b7
atik66 <48415208+atik66@users.noreply.github.com>
2022-05-31 10:57:13 +0600

    Add files via upload

---
+++ A - AtCoder Quiz 3.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using  namespace std;
+
+
+int main()
+{
+
+    string c,s;
+
+    int k;
+    cin>>k;
+
+    if(k>=42){
+
+      cout<<"AGC0"<<k+1<<endl;
+
+    }
+
+else{
+    if(k<=9){
+  cout<<"AGC00"<<k<<endl;
+}
+else{
+  cout<<"AGC0"<<k<<endl;
+}
+}
+  return 0;
+}
